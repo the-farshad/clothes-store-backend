c00a3f697ec93d59142f10618f0278ac0032b605
Farshad <Farshad.1991@gmail.com>
2021-05-26 23:46:47 +0300

    project dockerized!

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # clothes-store-backend
+
+Build the docker image form the Docker file with the below command:
+
+## 1
+* run that command in project directory:
+``` 
+sudo docker build . -t thefarshad/nodejs
+```
+
+## 2
+
+* And then Docker Compose run
+```
+sudo docker-compose up --build
+```
+
+> All of things ready
+
+Check api on port 8080
+
+Check phpMyAdmin on port 3306
+
+## 3
+* run that on phpMyAdmin
+
+```
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+03:00";
+
+-- Database: `mySQL_db`
+-- Table structure for table `ITEM_TABLES`
+
+CREATE TABLE `ITEM_TABLES` (
+  `ITEM_NAME` varchar(50) NOT NULL,
+  `ITEM_DESC` varchar(100) NOT NULL,
+  `ITEM_ONHAND` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table `ITEM_TABLES`
+
+INSERT INTO `ITEM_TABLES` (`ITEM_NAME`, `ITEM_DESC`, `ITEM_ONHAND`) VALUES
+('TEST-ITEM-1', 'TEST-ITEM-DESC-1', 10),
+('TEST-ITEM-2', 'TEST-ITEM-DESC-2', 20);
+COMMIT;
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+var mysql = require('mysql');
+var express = require('express');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+var responseStr = "MySQL Data:";
+
+app.get('/',function(req,res){
+   
+   var mysqlHost = process.env.MYSQL_HOST || 'mySQL-host';
+   var mysqlPort = process.env.MYSQL_PORT || '3306';
+   var mysqlUser = process.env.MYSQL_USER || 'mySQL-username';
+   var mysqlPass = process.env.MYSQL_PASS || 'SQL-password';
+   var mysqlDB   = process.env.MYSQL_DB   || 'mySQL_db';
+   console.log(mysqlHost ,mysqlPort, mysqlUser, mysqlPass, mysqlDB)
+
+   var connectionOptions = {
+     host: mysqlHost,
+     port: mysqlPort,
+     user: mysqlUser,
+     password: mysqlPass,
+     database: mysqlDB
+   };
+
+   
+   console.log('MySQL Connection config:');
+   console.log(connectionOptions);
+
+   var connection = mysql.createConnection(connectionOptions);
+
+   connection.connect();
+ 
+   connection.query('SELECT * FROM ITEM_TABLES', function (error, results, fields) {
+     if (error) throw error;
+     
+     responseStr = '';
+
+     results.forEach(function(data){
+        responseStr += data.ITEM_NAME + ' : ';
+        console.log(data);
+     });
+
+     if(responseStr.length == 0)
+        responseStr = 'No records found';
+
+     console.log(responseStr);
+
+     res.status(200).send(responseStr);
+   });
+    
+   connection.end();
+});
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
